8aa72500e9c730b6de7659b3986b93717268a5c8
roquemgc <roquemgcosta@gmail.com>
2020-11-12 14:36:23 -0300

    new functions manterPeca.js

--- views/js/game.js
+++ views/js/game.js
@@ -17,21 +17,23 @@ document.onkeydown = checarTecla;
 function checarTecla(e) {
 
     e = e || window.event;
-
+    // 38 = seta pra cima
     if (e.keyCode == '38') {
       console.log('cima')
     }
+    // 40 = seta pra baixo
     else if (e.keyCode == '40') {
 			console.log('baixo')
     }
+    // 37 = seta pra esquerda
     else if (e.keyCode == '37') {
       console.log('esquerda')
     }
+    // 39 = seta pra direita
     else if (e.keyCode == '39') {
       console.log('direita')
     }
 }
-
 
 function verificarColisao() {
 	
--- views/js/manterPeca.js
+++ views/js/manterPeca.js
@@ -1,13 +1,17 @@
 "use strict";
-
+// Gira a peça em jogo no sentido horário
 function girarPeca() {
 
 }
-
+// Move a peça em jogo para esquerda
 function moverPecaPraEsquerda() {
 
 }
-
+// Move a peça em jogo para a direita
 function moverpecaPecaPraDireita() {
-  
+
+}
+// Acelera a queda da peça em jogo
+function acelerarPeca() {
+
 }
