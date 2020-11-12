f2fee21f2f1123330a2848709723e519beb5fb53
Mateus Ferraz <mateusferrazf4@gmail.com>
2020-11-12 10:45:15 -0300

    function 'getMatrizVaziaTabuleiro()'

--- views/js/game.js
+++ views/js/game.js
@@ -2,10 +2,22 @@
 
 function startGame(largura,altura){
 	try {
+		var tabuleiro = getMatrizVaziaTabuleiro(largura,altura);
 		gerarTabuleiro(largura,altura);
 		var peca = new Peca(5);
 		printarPeca(peca);
 	} catch(e){
 		console.log(e);
 	}
+}
+
+function getMatrizVaziaTabuleiro(largura,altura){
+	var tabuleiro = new Array(altura);
+	for(var i=0; i<altura; i++){
+		tabuleiro[i] = new Array(largura);
+		for(var j=0; j<largura; j++){
+			tabuleiro[i][j] = 0;
+		}
+	}
+	return tabuleiro;
 }
