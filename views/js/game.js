f7e78a81c63b2ac761f6fe0844532d8fee44886f
Alkzv <33153364+Alkzv@users.noreply.github.com>
2020-11-20 13:18:06 -0300

    Adicional (bônus) do modo de movimentar peças após o giro do tabuleiro

--- views/js/game.js
+++ views/js/game.js
@@ -2,48 +2,49 @@
 
 var isRightSpin = true;
 var needAdaptationOfTheMovementsOfThePieces = false;
+var changeTheKeys = false;
 
-function startGame(largura, altura){
-	try {
-
+function startGame(largura, altura) {
+    try {
+        changeTheKeys = document.querySelector('#toggleInput').checked;
         var tabuleiro = getMatrizVaziaTabuleiro(largura, altura);
         var peca = gerarPecaAleatoria(tabuleiro);
         //var peca = new Peca(tabuleiro,1);
         addPecaNaMatrizTabuleiro(tabuleiro, peca);
         printarTabuleiro(tabuleiro);
-        document.onkeydown = function(){ checarTecla(tabuleiro, peca); };
-    	temporizador();
-        rodada(tabuleiro,peca);
-	} catch(error){
-		console.log(error);
-	}
+        document.onkeydown = function () { checarTecla(tabuleiro, peca); };
+        temporizador();
+        rodada(tabuleiro, peca);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-function rodada(tabuleiro,peca){
+function rodada(tabuleiro, peca) {
     // Chama a função peca.moverBaixo() em intervalos de 1000 milisegundos
     var delayQueda = 1000;
     var quedaPeca = setInterval(() => {
-        acelerarPeca(tabuleiro,peca);
-        if(pecaColidiu(peca)){
+        acelerarPeca(tabuleiro, peca);
+        if (pecaColidiu(peca)) {
             clearInterval(quedaPeca);
             peca = gerarPecaAleatoria(tabuleiro);
             //peca = new Peca(tabuleiro,6);
             addPecaNaMatrizTabuleiro(tabuleiro, peca);
             printarTabuleiro(tabuleiro);
-            document.onkeydown = function(){ checarTecla(tabuleiro, peca); };
-            rodada(tabuleiro,peca);
+            document.onkeydown = function () { checarTecla(tabuleiro, peca); };
+            rodada(tabuleiro, peca);
         }
     }, delayQueda);
 }
 
-function temporizador(){
-        
+function temporizador() {
+
     const segundo = 1000;
     const minuto = segundo * 60;
     const hora = minuto * 60;
     const inicio = Date.now();
 
-    setInterval(function(){
+    setInterval(function () {
         const agora = Date.now() - inicio;
         var minutos = Math.floor((agora % hora) / minuto);
         var segundos = Math.floor((agora % minuto) / segundo);
@@ -52,104 +53,104 @@ function temporizador(){
 }
 
 function checarTecla(tabuleiro, peca) {
-	if(!(Peca.isPecaObj(peca))){
-		throw "'peca' não é um objeto da classe 'Peca'\nfunction 'checarTecla' - game.js";
-	}
+    if (!(Peca.isPecaObj(peca))) {
+        throw "'peca' não é um objeto da classe 'Peca'\nfunction 'checarTecla' - game.js";
+    }
     var e = e || window.event;
+
     if (e.keyCode == '38') {
         // Cima
         e.preventDefault();
 
-        if(needAdaptationOfTheMovementsOfThePieces){
+        if (needAdaptationOfTheMovementsOfThePieces && !changeTheKeys) {
 
-            acelerarPeca(tabuleiro,peca);
+            acelerarPeca(tabuleiro, peca);
 
-        }else{
+        } else {
 
-            girarPeca(tabuleiro,peca);
+            girarPeca(tabuleiro, peca);
 
         }
-        
+
     }
     else if (e.keyCode == '40') {
         // Baixo
         e.preventDefault();
 
-        if(needAdaptationOfTheMovementsOfThePieces){
+        if (needAdaptationOfTheMovementsOfThePieces && !changeTheKeys) {
 
-            girarPeca(tabuleiro,peca);
+            girarPeca(tabuleiro, peca);
 
-        }else{
+        } else {
 
-            acelerarPeca(tabuleiro,peca);
+            acelerarPeca(tabuleiro, peca);
 
         }
     }
     else if (e.keyCode == '37') {
         // Esquerda
         e.preventDefault();
-        
-        if(needAdaptationOfTheMovementsOfThePieces){
 
-            moverpecaPecaPraDireita(tabuleiro,peca);
+        if (needAdaptationOfTheMovementsOfThePieces && !changeTheKeys) {
 
-        }else{
+            moverpecaPecaPraDireita(tabuleiro, peca);
 
-            moverPecaPraEsquerda(tabuleiro,peca);
+        } else {
+
+            moverPecaPraEsquerda(tabuleiro, peca);
 
         }
-       
+
     }
     else if (e.keyCode == '39') {
         // Direita
         e.preventDefault();
 
-        if(needAdaptationOfTheMovementsOfThePieces){
+        if (needAdaptationOfTheMovementsOfThePieces && !changeTheKeys) {
 
-            moverPecaPraEsquerda(tabuleiro,peca);
-            
-        }else{
+            moverPecaPraEsquerda(tabuleiro, peca);
 
-            moverpecaPecaPraDireita(tabuleiro,peca);
+        } else {
+
+            moverpecaPecaPraDireita(tabuleiro, peca);
 
         }
-        
+
     }
 }
 
 function aumentarPontuacao(linhaRemovidas) {
-  var elemPontuacao = document.getElementById("pontuacao")
-  
-  elemPontuacao.innerText = parseInt(elemPontuacao.value) + ((linhaRemovidas * 10) + (10 * linhaRemovidas -1))
-	aumentarDificuldade();
+    var elemPontuacao = document.getElementById("pontuacao")
+
+    elemPontuacao.innerText = parseInt(elemPontuacao.value) + ((linhaRemovidas * 10) + (10 * linhaRemovidas - 1))
+    aumentarDificuldade();
 }
 
 function aumentarDificuldade() {
-	dif = elemPontuacao / 300;
-	delayQueda = (-50) * dif;
-	if (delayQueda < 10) {
-		delayQueda = 10;
-	}
-	document.getElementById("nivel-dificuldade").innerHTML = dif;
+    dif = elemPontuacao / 300;
+    delayQueda = (-50) * dif;
+    if (delayQueda < 10) {
+        delayQueda = 10;
+    }
+    document.getElementById("nivel-dificuldade").innerHTML = dif;
 }
 
-function actionSpinningGame(){
+function actionSpinningGame() {
 
-    var animationSpinningGame = document.getElementById('rolling-tetris'); 
+    var animationSpinningGame = document.getElementById('rolling-tetris');
 
-    if(isRightSpin){
+    if (isRightSpin) {
 
         animationSpinningGame.classList.toggle('spinningRollingTetris');
-        isRightSpin = !isRightSpin; 
+        isRightSpin = !isRightSpin;
         needAdaptationOfTheMovementsOfThePieces = !needAdaptationOfTheMovementsOfThePieces;
 
 
-    }else{
+    } else {
 
         animationSpinningGame.classList.toggle('backToNormalRollingTetris');
-        isRightSpin = !isRightSpin; 
+        isRightSpin = !isRightSpin;
         needAdaptationOfTheMovementsOfThePieces = !needAdaptationOfTheMovementsOfThePieces;
-    
-    }
 
+    }
 }
