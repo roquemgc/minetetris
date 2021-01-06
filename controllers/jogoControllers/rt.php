8f13554d88037e4390aba0b540668b6e804954dc
Alkzv <33153364+Alkzv@users.noreply.github.com>
2021-01-06 09:54:52 -0300

    eliminando inutilidades no rt.php

--- controllers/jogoControllers/rt.php
+++ controllers/jogoControllers/rt.php
@@ -5,8 +5,6 @@ require_once '..\..\models\Jogo.php';
 require_once '..\db\dbConnection.php';
 require_once '..\..\models\DAO\JogoDAO.php';
 require_once '..\services\errors.php';
-require_once '..\..\models\Jogador.php';
-require_once '..\..\models\DAO\JogadorDAO.php';
 
 session_start();
 
@@ -17,7 +15,6 @@ try {
         $username = $_SESSION["usuario"];
         $jogo = new Jogo($username, $_POST['tempo'], $_POST['dificuldade'], $_POST['pontuacao'], $_POST['linhas_eliminadas']);
         JogoDAO::insert(getNewDBConnection(), $jogo);
-        returnSuccessToPage("Dados salvos com sucesso", "rt.php");
     }
 } catch (Exception $e) {
 
